37234f49389aa50fc219b507e3fbaaac9a0576e4
Jeremy Castle <x95castle1@yahoo.com>
2024-04-24 11:52:19 -0500

    Team 1 Branch

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + Mae!";
+		return "Greetings from Spring Boot + Tanzu + Team1";
 	}
 
 }
