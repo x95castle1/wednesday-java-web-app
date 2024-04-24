23160fb91dbc6fb4c436d7ad0cb690dbe3744ce4
Jeremy Castle <x95castle1@yahoo.com>
2024-04-24 11:53:58 -0500

    Team 1 Branch

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu + Team2"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu + Team1"));
     }
 }
