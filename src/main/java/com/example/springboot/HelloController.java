bc57f593728949dbb469ef6af84cf96a8167d8c6
Jeremy Castle <x95castle1@yahoo.com>
2024-04-24 08:52:08 -0500

    Updated the tests to include Mae LARGE

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + JC6";
+		return "Greetings from Spring Boot + Tanzu + Mae!";
 	}
 
 }
